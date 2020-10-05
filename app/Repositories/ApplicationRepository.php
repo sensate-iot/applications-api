6326bab40659207f50bee1ed15896fe9a69b6781
Michel Megens <michel@michelmegens.net>
2020-10-05 22:08:19 +0200

    app: json: update JSON return values
    Always return arrays instead of objects.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- app/Repositories/ApplicationRepository.php
+++ app/Repositories/ApplicationRepository.php
@@ -9,23 +9,28 @@ class ApplicationRepository implements IApplicationInterface
 {
     public function all(): array
     {
-        $data = Application::all()->sortBy('ranking');
-
-        foreach($data as $entry) {
-            $entry->roles = $entry->roles();
-        }
-
-        return $data;
+        return Application::query()->get()->toArray();
     }
 
     public function findByName(string $name): array
     {
         $data = Application::query()->where('name', 'LIKE', $name);
+
+        if ($data === null) {
+            return [];
+        }
+
         return $data->get()->all();
     }
 
-    public function findOne(string $name): Model
+    public function findOne(string $name): ?Model
     {
-        return Application::query()->where('name', '=', $name)->get()->first();
+        $data = Application::query()->where('name', '=', $name)->get()->first();
+
+        if($data === null) {
+            return null;
+        }
+
+        return $data;
     }
 }
--- app/Repositories/IApplicationInterface.php
+++ app/Repositories/IApplicationInterface.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 interface IApplicationInterface
 {
     function all(): array;
-    function findOne(string $name): Model;
+    function findOne(string $name): ?Model;
     function findByName(string $name): array;
 }
